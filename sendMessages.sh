e6267f8e0b0237b5338124f898a22e04f4a85bff
Matt Clement <mallize@gmail.com>
2018-04-10 12:37:42 -0500

    update message

--- sendMessages.sh
+++ sendMessages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo "Sending message to $line..."
-    osascript SendSMS.app $line "Thank you for attending the Easter Egg Hunt 2018! Please take a moment to let us know how we did: tinyurl.com/h20egghunt"
+    osascript SendSMS.app $line "Test Message"
 done < "$1"
